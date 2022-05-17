7b7b4396b66a76bbe285fd065210832c33fbf438
Raul Batisteli Fiorin <52724251890@info.scs>
2022-05-17 16:03:22 -0300

    .

--- src/Pages/gerenciamento.js
+++ src/Pages/gerenciamento.js
@@ -14,32 +14,57 @@ import "../../src/Assets/css/style.css";
 
 
 
-
 export default function Gereciamento() {
+    // const [selectedFile, setSelectedFile] = useState();
+	// const [isFilePicked, setIsFilePicked] = useState(false);
+
+	// const changeHandler = (event) => {
+	// 	setSelectedFile(event.target.files[0]);
+	// 	setIsSelected(true);
+	// };
+
+	// const handleSubmission = () => {
+	// 	const formData = new FormData();
+
+	// 	formData.append('File', selectedFile);
+
+	// 	fetch(
+	// 		'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
+	// 		{
+	// 			method: 'POST',
+	// 			body: formData,
+	// 		}
+	// 	)
+	// 		.then((response) => response.json())
+	// 		.then((result) => {
+	// 			console.log('Success:', result);
+	// 		})
+	// 		.catch((error) => {
+	// 			console.error('Error:', error);
+	// 		});
+	// };
+	// };
 
     return (
         <div>
             <Header />
-            <main>
-               
-               <section>
-                   
-                   <input className="input-banner" type="file"id="file-browser-input" name="file-browser-input" 
-                   ref={input=>this.fileInput=input}
-                   onDragOver={(e)=>{
-                           e.preventDefault();
-                       e.stopPropagation();
-                       }}
-                       onDrop={this.onFileLoad.bind(this)}
-                       onChange={this.onFileLoad.bind(this)}
-                       />
-               </section>
-               <div className="container_img">
-               <input className="input-imagem" type="file"/>
-               <input className="input-imagem" type="file"/>
-               <input className="input-imagem" type="file"/>
-               </div>
-           </main>
+            {/* <input type="file" name="file" onChange={changeHandler} />
+			{isSelected ? (
+				<div>
+					<p>Filename: {selectedFile.name}</p>
+					<p>Filetype: {selectedFile.type}</p>
+					<p>Size in bytes: {selectedFile.size}</p>
+					<p>
+						lastModifiedDate:{' '}
+						{selectedFile.lastModifiedDate.toLocaleDateString()}
+					</p>
+				</div>
+			) : (
+				<p>Select a file to show details</p>
+			)}
+			<div>
+				<button onClick={handleSubmission}>Submit</button>
+			</div> */}
 
             <Footer />
         </div>
--- src/Pages/login.js
+++ src/Pages/login.js
@@ -10,7 +10,7 @@ export default function Login() {
     return (
         <div>
             <Header />
-            
+            <a  href="/Gerenciamento">Login</a>
         </div>
     )
 }
