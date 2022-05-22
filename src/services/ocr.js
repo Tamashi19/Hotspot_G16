f484d625522d60d3b54c244e3e77b814ada3c7a9
Tamashi19 <29raul19@gmail.com>
2022-05-21 21:59:32 -0300

    .

--- src/services/ocr.js
+++ src/services/ocr.js
@@ -1,41 +1,41 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const LerConteudoDaImagem = async (formData) => {
+export const LerConteudoDaImagem = async (formData) => {
 
-//     let resultado;
-//     await axios({
-//         method : "POST",
-//         url : "",
-//         data : formData,
-//         headers : {
-//             "Content-Type" : "multipart/form-data",
-//             "Ocp-Apim-Subscription-Key" : "c3d4e190149044b585f84c6465dd2ac5"
-//         }
-//     })
-//     .then(response => {
-//         // console.log(response)
-//         resultado = FiltrarOBJ(response.data);
-//     })
-//     .catch(erro => console.log(erro))
+    let resultado;
+    await axios({
+        method : "POST",
+        url : "",
+        data : formData,
+        headers : {
+            "Content-Type" : "multipart/form-data",
+            "Ocp-Apim-Subscription-Key" : "c3d4e190149044b585f84c6465dd2ac5"
+        }
+    })
+    .then(response => {
+        // console.log(response)
+        resultado = FiltrarOBJ(response.data);
+    })
+    .catch(erro => console.log(erro))
 
-//     return resultado;
-// }
+    return resultado;
+}
 
 
-// export const FiltrarOBJ = (obj) => {
+export const FiltrarOBJ = (obj) => {
 
-//     let resultado;
+    let resultado;
 
-//     obj.regions.forEach(region => {
-//         region.lines.forEach(line => {
-//             line.words.forEach(word => {
-//                 if(word.text[0] === "1" && word.text[1] === "2"){
-//                     resultado = word.text;
-//                 }
-//             });
-//         });
-//     });
+    obj.regions.forEach(region => {
+        region.lines.forEach(line => {
+            line.words.forEach(word => {
+                if(word.text[0] === "1" && word.text[1] === "2"){
+                    resultado = word.text;
+                }
+            });
+        });
+    });
 
-//     return resultado;
+    return resultado;
 
-// }
+}
